5ad3c157a857d9c0c537ac02e9a9356cb19e3639
sergiocorato <sergiocorato@gmail.com>
2019-07-25 09:27:20 +0200

    [FIX] entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,42 +2,12 @@
 
 set -e
 
+sed -i "/^admin_passwd/c\admin_passwd=$ADMIN_PASSWD" $ODOO_CONF
 
-# set the postgres database host, port, user and password according to the environment
-# and pass them as arguments to the openerp process if not present in the config file
-: ${HOST:=${DB_PORT_5432_TCP_ADDR:='database'}}
-: ${PORT:=${DB_PORT_5432_TCP_PORT:=5432}}
-: ${USER:=${DB_ENV_POSTGRES_USER:=${POSTGRES_USER:='openerp'}}}
-: ${PASSWORD:=${DB_ENV_POSTGRES_PASSWORD:=${POSTGRES_PASSWORD:='openerp'}}}
+ln -s /etc/supervisor/supervisord.conf /etc/supervisord.conf
 
-DB_ARGS=()
-function check_config() {
-    param="$1"
-    value="$2"
-    if ! grep -q -E "^\s*\b${param}\b\s*=" "$OPENERP_SERVER" ; then
-	DB_ARGS+=("--${param}")
-	DB_ARGS+=("${value}")
-    fi;
-}
-check_config "db_host" "$HOST"
-check_config "db_port" "$PORT"
-check_config "db_user" "$USER"
-check_config "db_password" "$PASSWORD"
-
-case "$1" in
-    -- | openerp-server)
-	shift
-	if [[ "$1" == "scaffold" ]] ; then
-	    exec python /opt/openerp/server/openerp-server  "$@"
-	else
-	    exec python /opt/openerp/server/openerp-server  "$@" "${DB_ARGS[@]}"
-	fi
-	;;
-    -*)
-	exec python /opt/openerp/server/openerp-server  "$@" "${DB_ARGS[@]}"
-	;;
-    *)
-	exec "$@"
-esac
-
-exit 1
+if [ -z "$@" ]; then
+  exec /usr/local/bin/supervisord -c /etc/supervisord.conf --nodaemon
+else
+  exec PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $@
+fi
