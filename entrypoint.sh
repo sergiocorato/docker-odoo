a14912ba7362bfd38e496a557f84dda64bfa5fb2
sergiocorato <sergiocorato@gmail.com>
2019-07-25 23:06:36 +0200

    [FIX] entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@ sed -i "/^admin_passwd/c\admin_passwd=$ADMIN_PASSWD" "$ODOO_CONF"
 
 ln -s /etc/supervisor/supervisord.conf /etc/supervisord.conf
 
-if [ -z "$@" ]; then
+if [ -z "$*" ]; then
   exec /usr/local/bin/supervisord -c /etc/supervisord.conf --nodaemon
 else
-  exec PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $@
+  exec PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin "$@"
 fi
 exec /usr/local/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf --nodaemon
