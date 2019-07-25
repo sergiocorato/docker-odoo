ec76be7493bbd807f98f6ca3d06edfcec9e93636
sergiocorato <sergiocorato@gmail.com>
2019-07-25 15:24:07 +0200

    [FIX] revert

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,9 @@ set -e
 
 sed -i "/^admin_passwd/c\admin_passwd=$ADMIN_PASSWD" "$ODOO_CONF"
 
-if [ -z "$*" ]; then
+if [ -z "$@" ]; then
   exec /usr/local/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf --nodaemon
 else
-  exec PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin "$@"
+  exec PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $@
 fi
+exec /usr/local/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf --nodaemon
