11a4667f2f015615e2b963aa30dec67c77f6ad75
sergiocorato <sergiocorato@gmail.com>
2019-07-25 14:14:02 +0200

    [FIX] use var

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+sed -i "/^admin_passwd/c\admin_passwd=$ADMIN_PASSWD" "$ODOO_CONF"
+
 if [ -z "$*" ]; then
   exec /usr/local/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf --nodaemon
 else
