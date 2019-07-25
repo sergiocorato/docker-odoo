e682a432f12c99edc01f23381fefafb29462bea9
sergiocorato <sergiocorato@gmail.com>
2019-07-25 12:59:41 +0200

    [FIX]

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-sed -i "/^admin_passwd/c\admin_passwd=$ADMIN_PASSWD" $ODOO_CONF
-sed -i "/^addons_path/c\addons_path=$ADDONS_PATH" $ODOO_CONF
+sed -i "/^admin_passwd/c\admin_passwd=$ADMIN_PASSWD" "$ODOO_CONF"
+sed -i "/^addons_path/c\addons_path=$ADDONS_PATH" "$ODOO_CONF"
 
-if [ -z "$@" ]; then
-  exec /usr/local/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf --nodaemon
-else
-  exec PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin $@
-fi
+exec /usr/local/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf --nodaemon
