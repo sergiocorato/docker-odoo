e8239378874340255ea1675f74d9676b6a83a4f3
sergiocorato <sergiocorato@gmail.com>
2019-07-26 22:32:54 +0200

    [FIX] remove ln

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-ln -s /etc/supervisor/supervisord.conf /etc/supervisord.conf
-
 if [ -z "$*" ]; then
-  exec /usr/local/bin/supervisord -c /etc/supervisord.conf --nodaemon
+  exec /usr/local/bin/supervisord -c /etc/supervisor/supervisord.conf --nodaemon
 else
   exec PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin "$@"
 fi
