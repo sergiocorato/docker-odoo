8189177aa1ae0eece720f0a9559c5e1ed2a2d0ec
sergiocorato <sergiocorato@gmail.com>
2019-08-19 12:00:28 +0200

    [FIX] odoo service

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ if [ -f $REQ_FILE ]; then
 fi
 
 if [ -f $UPD_FILE ]; then
-    /opt/odoo/openerp-server --config=$ODOO_CONF -u $(< $UPD_FILE) -d $ODOO_DB --load-language=it_IT --i18n-overwrite --workers=0 --stop-after-init
+    /usr/local/bin/odoo --config=$ODOO_CONF -u $(< $UPD_FILE) -d $ODOO_DB --load-language=it_IT --i18n-overwrite --workers=0 --stop-after-init
     mkdir -p $ODOO_DATADIR/setup
     mv $UPD_FILE $ODOO_DATADIR/setup/updated-modules-on-`date +%y%m%d`-at-`date +%H%M%S`.txt
 fi
