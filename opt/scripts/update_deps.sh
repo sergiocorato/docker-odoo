eb8708406f717a9a2fbe447d149ae48c134b87dc
sergiocorato <sergiocorato@gmail.com>
2026-07-14 14:47:28 +0200

    [WIP] remove build isolation

--- opt/scripts/update_deps.sh
+++ opt/scripts/update_deps.sh
@@ -31,7 +31,6 @@ if [ ! -f "$ODOO_REQ_FILE" ]; then
     echo "No requirements file, skipping deps update."
 else
     echo "Updating requirements..."
-    uv_install setuptools wheel build setuptools-odoo
     uv_install --reinstall --index-strategy unsafe-best-match -r "$ODOO_REQ_FILE"
 
     NOW="$(date +%y%m%d_%H%M%S)"
